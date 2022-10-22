3af80338fb4308e5171ccde519cbf0b81ba027ec
Ramesh Mandaleeka <ramesh.mandaleeka@gmail.com>
2022-10-21 23:59:01 -0400

    Renamed the class to give it a more mearningful name

--- src/main/java/com/ascendant76/redis/RedisServerLauncher.java
+++ src/main/java/com/ascendant76/redis/RedisServerLauncher.java
@@ -3,7 +3,7 @@ package com.ascendant76.redis;
 
 import redis.embedded.RedisServer;
 
-public class RedisLauncher {
+public class RedisServerLauncher {
     public static void main(String[] args) {
         RedisServer.builder().port(6379).build().start();
     }
